488d21cf6898a1514b7641d965d8cbb74ae05b73
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-24 14:22:13 -0600

    Added for loops for rest of coin denominations.

--- 001/change_calc.js
+++ 001/change_calc.js
@@ -7,6 +7,15 @@ function changer(amount){
 	for(;0.24 < amount; q ++){
 		amount = amount - 0.25;
 	}
+		for(;0.09 < amount; d ++){
+		amount = amount - 0.10;
+	}
+		for(;0.04 < amount; n ++){
+		amount = amount - 0.05;
+	}
+		for(;0 < amount; p ++){
+		amount = amount - 0.01;
+	}
 	return amount
 }
 console.log(changer(4.17));
